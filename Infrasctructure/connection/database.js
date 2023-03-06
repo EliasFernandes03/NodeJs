53c06854173efa8bb0a363e8a76c72b7ca4263d6
EliasFernandes02 <eliasneto0014@gmail.com>
2023-03-06 14:01:28 -0300

    create the connection to database

---
+++ Infrasctructure/connection/database.js
@@ -0,0 +1,30 @@
+
+const mysql = require('mysql2/promise');
+
+const USER_NAME = 'root';
+const USER_PASS = '1234';
+const DB_NAME = 'db_points';
+const DB_HOST = 'localhost';
+
+async function connect() {
+    const con = await mysql.createConnection({
+         user: USER_NAME,
+         password: USER_PASS,
+         host: DB_HOST,
+         database: DB_NAME
+    });
+
+    return con;
+}
+
+async function execute(sql) {
+    const connection = await connect();
+
+    const [rows, fields] = await connection.execute(sql);
+
+    return rows;
+}
+
+module.exports = {
+    execute
+};
