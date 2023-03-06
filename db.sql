51f7b0e9182f81cb6f33db18b0b5a464266e700e
EliasFernandes02 <eliasneto0014@gmail.com>
2023-03-06 14:15:25 -0300

    add sql to create the db an tb

---
+++ db.sql
@@ -0,0 +1,18 @@
+
+CREATE DATABASE db_points;
+
+USE db_points;
+
+CREATE TABLE tb_file (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+);
+
+INSERT INTO tb_file 
+    (id, name)
+VALUES 
+    (1, 'file1'),
+    (2, 'file2'),
+    (3, 'file3'),
+
+
